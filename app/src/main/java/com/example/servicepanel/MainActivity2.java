3faa298854e10793ae15877edfddd3f4fe5c8018
bartosz1998 <bartoszbizon1998@gmail.com>
2022-11-30 11:22:17 +0100

    Added recycler view

--- app/src/main/java/com/example/servicepanel/MainActivity2.java
+++ app/src/main/java/com/example/servicepanel/MainActivity2.java
@@ -1,14 +1,42 @@
 package com.example.servicepanel;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
-public class MainActivity2 extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity2 extends AppCompatActivity {
+    private ArrayList<User> usersList;
+    private RecyclerView recyclerView;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        recyclerView = findViewById(R.id.tabeleView);
+        usersList = new ArrayList<>();
+
+        setUserInfo();
+        setAdapter();
+    }
+
+    private void setAdapter(){
+        recyclerAdapter adapter = new recyclerAdapter(usersList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void setUserInfo(){
+        usersList.add(new User("john"));
+        usersList.add(new User("bob"));
+        usersList.add(new User("Aliece"));
     }
 }
