a1dc0bc8ed618bf37e57d976dc4c913cf831e610
bartosz1998 <bartoszbizon1998@gmail.com>
2022-12-06 21:00:25 +0100

    Function Add event

--- app/src/main/java/com/example/servicepanel/AddNewUserActivity.java
+++ app/src/main/java/com/example/servicepanel/AddNewUserActivity.java
@@ -4,13 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.servicepanel.db.AppDatabase;
 import com.example.servicepanel.db.DataEvent;
 
 public class AddNewUserActivity extends AppCompatActivity {
+
+    String[] items = {"OCZEKUJE NA REALIZACJE","ZAKOŃCZONO","BĘDZIE KONTYNUOWANE","W TRAKCIE"};
+
+    AutoCompleteTextView autoCompleteTxt;
+
+    ArrayAdapter<String> adapterItems;
+
+    String item;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +35,24 @@ public class AddNewUserActivity extends AppCompatActivity {
         final EditText saveStatusEvent = findViewById(R.id.saveStatusEvent);
 
         Button saveButton = findViewById(R.id.saveButton);
+
+        autoCompleteTxt = findViewById(R.id.auto_complete_txt);
+
+        adapterItems = new ArrayAdapter<String>(this,R.layout.list_item,items);
+
+        autoCompleteTxt.setAdapter(adapterItems);
+
+        autoCompleteTxt.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                 item = parent.getItemAtPosition(position).toString();
+            }
+        });
+
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                saveNewUser(saveNameEvent.getText().toString(), saveStatusEvent.getText().toString());
+                saveNewUser(saveNameEvent.getText().toString(), item);
             }
         });
 
