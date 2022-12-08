cda4699757ca453f7d7b9225002211c48aeb8466
bartosz1998 <bartoszbizon1998@gmail.com>
2022-12-08 09:44:40 +0100

    Added progressBar to action list

--- app/src/main/java/com/example/servicepanel/ListAction.java
+++ app/src/main/java/com/example/servicepanel/ListAction.java
@@ -3,20 +3,33 @@ package com.example.servicepanel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class ListAction extends AppCompatActivity {
+
+    private ProgressBar circular_pro;
+    private TextView status;
+
+    private int progressStatus = 50;
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_action);
 
-        TextView nameTxt = findViewById(R.id.nameTextView);
+        circular_pro=(ProgressBar) findViewById(R.id.progressbar_circular);
+        status=(TextView) findViewById(R.id.text_status);
+
+        circular_pro.setProgress(progressStatus);
+        status.setText(progressStatus+"%");
+        /*TextView nameTxt = findViewById(R.id.nameTextView);
 
         Bundle bundle = getIntent().getExtras();
         String name = bundle.getString("name");
 
-        nameTxt.setText(name);
+        nameTxt.setText(name);*/
     }
 }
